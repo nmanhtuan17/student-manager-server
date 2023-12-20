90204db754e19e6a79f2029a0f896bfd85b7e26d
Tuwns <manhtuan17403@gmail.com>
2023-12-20 22:51:20 +0700

    fix env

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express")
 const cors = require("cors")
-const dotenv = require("dotenv")
 const initRoute = require('./src/Routes/initRoute')
 const connect = require('./src/Config/db/index')
 
 
 const app = express()
-dotenv.config()
 app.use(cors())
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -15,7 +13,7 @@ app.use((req, res, next) => {
 })
 
 
-const port = process.env.PORT || 8080
+const port = 8080
 app.use(express.json())
 app.use(express.urlencoded({
   extended: true
--- src/Config/db/index.js
+++ src/Config/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-const url = process.env.MONGO_URL
+const url = "mongodb+srv://cnpm-stdm:ohISsPVqCAeCAWMh@student-management.rs2ajjf.mongodb.net/"
 
 const connect = () => {
     mongoose.connect(url)
