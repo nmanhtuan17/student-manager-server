5ce169032639d8a0c5788de286d34de8d3737b11
Tuwns <manhtuan17403@gmail.com>
2024-01-19 22:33:28 +0700

    fix cors

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const cookieParser = require('cookie-parser')
 
 const app = express()
 app.use(cors())
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-})
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+//   next();
+// })
 app.use(cookieParser());
 
 const port = process.env.PORT
