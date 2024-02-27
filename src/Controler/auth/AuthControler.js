0dafc30c2eeca91bc67915b9c8bb625fad762869
tuanmn <manhtuan17403@gmail.com>
2024-02-27 20:22:24 +0700

    update change pass

--- src/Controler/auth/AuthControler.js
+++ src/Controler/auth/AuthControler.js
@@ -68,19 +68,23 @@ module.exports = {
   },
 
   changePassword: async (req, res) => {
-    const { password } = req.body
-    const hashPassword = await Encrypt.cryptPassword(password)
+    const { password, newPass } = req.body
+    const hashPassword = await Encrypt.cryptPassword(newPass)
     try {
       const user = await User.findById(req.user.id);
       if (!user) {
         res.status(404).json({ message: "user not exist" })
         return;
       }
+      const comparePassword = await Encrypt.comparePassword(password, user.password)
+      if(!comparePassword) {
+        return res.status(400).json({ message: 'password is not correct' });
+      }
       user.password = hashPassword;
       await user.save();
-      return res.status(200).json({ message: "Update success!!!" })
+      return res.status(200).json({ message: "Update success!" })
     } catch (e) {
-      res.status(500).json({ message: 'server error' })
+      res.status(500).json({ message: 'server error', error: e })
     }
   },
 
