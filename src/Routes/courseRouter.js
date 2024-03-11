ed093e10b2fa9a6350b5f35020684ce5823705dc
Tuwns <manhtuan17403@gmail.com>
2024-03-11 23:35:17 +0700

    fix enpoint

--- src/Routes/courseRouter.js
+++ src/Routes/courseRouter.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 const CourseControler = require('../Controler/courses/CourseControler')
 const middlewareControler = require('../MiddleWare/middlewareControler')
 
-router.get('/getAll', middlewareControler.verifyToken, CourseControler.getAllCourse)
+router.get('/get-all-courses', middlewareControler.verifyToken, CourseControler.getAllCourse)
 router.get('/:id', middlewareControler.verifyToken, CourseControler.getCourse)
 router.post('/add-course', middlewareControler.verifyTokenIsAdmin, CourseControler.createCourse)
 router.post('/delete/:id', middlewareControler.verifyTokenIsAdmin, CourseControler.deleteCourse)
