b3becf785fde59ca7824d03786d21eb572d536fe
Tuwns <manhtuan17403@gmail.com>
2024-03-11 23:37:04 +0700

    fix enpoint

--- src/Routes/userRouter.js
+++ src/Routes/userRouter.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 const UserControler = require('../Controler/user/UserControler')
 const middlewareControler = require('../MiddleWare/middlewareControler')
 
-router.get('/getAll',middlewareControler.verifyTokenIsAdmin, UserControler.getAllUser)
+router.get('/get-all-users',middlewareControler.verifyTokenIsAdmin, UserControler.getAllUser)
 router.get('/:id', middlewareControler.verifyToken, UserControler.getUser)
 router.post('/create-user', middlewareControler.verifyTokenIsAdmin, UserControler.createUser)
 router.post('/delete/:id', middlewareControler.verifyTokenIsAdmin, UserControler.deleteUser)
